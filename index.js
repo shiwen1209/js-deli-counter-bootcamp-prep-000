cd0ddbebea7b20e0a71b579a177d19f56754a2bf
Wendy She <whshiwen@gmail.com>
2018-05-21 14:56:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 var katzDeli=[]
 function takeANumber(katzDeliLine,name){katzDeliLine.push(name);return(`Welcome, `+`${name}`+`. You are number `+`${katzDeliLine.length}`+` in line.`)}
 function nowServing(katzDeliLine){if(katzDeliLine.length===0){return(`There is nobody waiting to be served!`);}else{return(`Currently serving ${katzDeliLine.shift()}.`);}}
-function currentLine(line){if(line.length===0){return(`The line is currently empty.`)}else{var katzDeliIndex=[]; for(let i=0;i<katzDeli.length;i++){katzDeliIndex.push(` ${(i+1)}. ${katzDeli[i]}`)};return(`The line is currently:${katzDeliIndex}.`)}}
+function currentLine(line){if(line.length===0){return(`The line is currently empty.`)}else{var katzDeliIndex=[]; for(let i=0;i<katzDeli.length;i++){katzDeliIndex.push(` ${(i+1)}. ${katzDeli[i]}`)};return(`The line is currently:${katzDeliIndex}`)}}
